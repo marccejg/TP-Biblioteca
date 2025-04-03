3fde212850e6264e048bb7e98dd346ab8f0843a4
marccejg <marcejgarcia@gmail.com>
2025-04-03 12:53:29 -0300

    orden

--- index.ts
+++ index.ts
@@ -2,26 +2,15 @@ console.clear();
 import * as fs from 'readline-sync';
 import { Libros } from "./libros";
 import { Biblioteca } from "./biblioteca";
-import { escuela } from '../../Clase 9/escuela';
-
-
-let listadoLibros = new Array()
-console.log(listadoLibros)
-let biblioteca = new Biblioteca ("Recreo",listadoLibros);
-
-
-
-let libro1
-libro1.nombreDeLibro()
-libro1 = new Libros(nombreLibro,tipoLibro)
-biblioteca.ingresarLibro(libro1)
-
-
-console.log(listadoLibros)
-
-
-
-
+let listadoLibros = new Array();
+console.log(listadoLibros);
+let biblioteca = new Biblioteca("Recreo", listadoLibros);
+let nombreLibro = fs.question("Ingrese el nombre del libro: ");
+let tipoLibro = fs.question("Ingrese el tipo del libro: ");
+let libro1 = new Libros(nombreLibro, tipoLibro);
+// Ingresa el libro en la biblioteca
+biblioteca.ingresarLibro(libro1);
+console.log(listadoLibros);
 
 
 
--- libros.ts
+++ libros.ts
@@ -6,11 +6,8 @@ export class Libros {
     //constructor
     constructor(pNombre: string, pTipo: string) {
         this.nombre = pNombre;
-        this.tipo = pTipo;
-
-       
+        this.tipo = pTipo;      
     };
-
     //get y set
     public getNombre() {
         return this.nombre;
@@ -24,13 +21,11 @@ export class Libros {
     public setTipo(pTipo: string): void {
         this.tipo = pTipo;
     }
-
     public nombreDeLibro(): {nombreLibro: string, tipoLibro: string} {
         //let idLibro: string = fs.question("Id: ") 
         let nombreLibro: string = fs.question("Nombre del libro: ")
         let tipoLibro: string = fs.question("que tipo de libro es: ")
-      
-            return {nombreLibro,tipoLibro};
+        return {nombreLibro,tipoLibro};
 }
 
 }
